968db2ec8d0bf96698c458af3f06aa66853319ab
Tofiq780065 <tuseegretuu.u@gmail.com>
2025-01-14 02:15:49 +0530

    Update soul1.py

--- soul1.py
+++ soul1.py
@@ -9,7 +9,7 @@ import base64
 
 active_processes = {}
 GITHUB_API_URL = "https://api.github.com"
-GITHUB_TOKEN = "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"
+GITHUB_TOKEN = "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"
 GITHUB_USERNAME = "Tofiq780065"
 
 def install_packages():
